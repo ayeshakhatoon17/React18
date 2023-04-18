5a0c8921b96c9ccf7d502313dd0041e4f9023090
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-04-18 10:05:11 +0000

    React

---
+++ my-app/src/books.js
@@ -0,0 +1,30 @@
+import img1 from './img/power.jpg'
+import img2 from "./img/hetoldme.jpg"
+import img3 from "./img/youcan'tjoke.jpg"
+
+
+ const books = [
+  
+    {
+      author: 'Robert Greene',
+      title: 'The 48 Laws of Power',
+      img: img1,
+      id: 1,
+    },
+  
+  {
+  author: 'Laura Dave',
+  title: 'The Last Thing He Told Me',
+  img: img2,
+  id: 2,
+  },
+
+  {
+    author: 'Kat Timpf',
+    title: "You Can't Joke About That",
+    img: img3,
+    id: 3,
+    },
+];
+
+export default books
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,88 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css'
-
-// Creating component 
-// function Greeting() {
-//     return(
-//         <div>
-//         <h2>My First Component</h2>;
-//   </div>
-
-//     )
-// }
-
-
-// another way to create a component
-//function Greeting() {
-
-//     return React.createElement('h3',{},'hello ayesha')
-//                                 element,{}, values 
-//  }
-
-
-
-
-// Nested Components
-//    function Greeting(){
-//        return <div>
-//            <Person/>
-//            <Message/>
-//        </div>
-//    }
-
-//    const Person = () => <h2>hey, ayesha here </h2>
-//    const Message = ()=>{
-//     return <p> iam happy</p>
-//    }
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Greeting />);
-
-
+import books from './books.js'
+import Book from './Book.js';
 
 // ********  BOOKS APP    ********
-const firstBook = {
-  author: 'Laura Dave',
-  title: 'The Last Thing He Told Me',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71LyuJP7yUL._AC_UL600_SR600,400_.jpg',
-};
-const secondBook = {
-  author: 'Robert Greene',
-  title: 'The 48 Laws of Power',
-  img: 'https://m.media-amazon.com/images/I/41KY-NORo9L._SX355_BO1,204,203,200_.jpg',
-};
 
-function BookList() {
+const BookList = () => {
+
   return (
     <section className='booklist'>
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
-}
-const Book = (props) => {
-  console.log(props);
-  return (
-    <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author} </h4>
-    </article>
-  );
 };
-  
-   
-  
+
+
   const root = ReactDOM.createRoot(document.getElementById('root'));
   
   root.render(<BookList />);
